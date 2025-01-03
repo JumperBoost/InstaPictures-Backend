fd198ca7ccbe1d909a15a99f4fd41dac177fffc5
JumperBoost <milwenn59@sfr.fr>
2025-01-03 10:53:39 +0100

    feat&fix: Optimisation du downloader + Ajout sécurité (CORS, vérif domaine et extension)

--- src/Controller/DownloadController.php
+++ src/Controller/DownloadController.php
@@ -9,14 +9,39 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DownloadController extends AbstractController {
+    private static $MEDIA_TYPES = ["jpg" => "image/jpeg", "png" => "image/png", "webp" => "image/webp", "heic" => "image/heic", "mp4" => "video/mp4"];
+    private static $AUTHORIZED_HOST = ["fbcdn.net", "cdninstagram.com"];
+
     #[Route('/download/{url}', requirements: ['url' => '.+'])]
     public function download(Request $request, mixed $url) {
-        $file = file_get_contents($url . '?' . $request->getQueryString());
-        $response = new Response($file);
-        $response->headers->set('Content-Disposition', 'attachment; filename="' . basename($url) . '";');
-        $response->sendHeaders();
-        return $response;
+        $type = static::$MEDIA_TYPES[pathinfo($url, PATHINFO_EXTENSION)] ?? null;
+        if(is_null($type))
+            return new Response("Le type du fichier demandé n'est pas autorisé.", 400);
+
+        $canDownload = array_reduce(static::$AUTHORIZED_HOST, fn($carry, $host) => $carry || str_ends_with(parse_url($url)['host'], $host), false);
+        if(!$canDownload)
+            return new Response("Le domaine du fichier demandé n'est pas autorisé.", 400);
+
+        // Ouvre une connexion HTTP au fichier distant
+        $context = stream_context_create(['http' => ['follow_location' => true]]);
+        $fp = fopen($url . '?' . $request->getQueryString(), 'rb', false, $context);
+        if (!$fp)
+            return new Response("Impossible de récupérer le fichier depuis l'hôte.", 500);
+
+        header("Content-Type: $type");
+        foreach ($http_response_header as $h) {
+            if (str_starts_with($h, 'Content-Length:')) {
+                header($h);
+                break;
+            }
+        }
+
+        fpassthru($fp);
+        fclose($fp);
+
+        return new Response();
     }
+
     #[Route('/download')]
     public function index(): Response {
         return new Response("Merci d'indiquer un URL.", 400);
